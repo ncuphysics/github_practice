f55880c612c679196e8f1531bccc66523a33592a
unknown <109222014@cc.ncu.edu.tw>
2023-02-08 00:33:34 +0800

    order drink function

--- Main.py
+++ Main.py
@@ -1,9 +1,11 @@
 from discord.ext          import commands
-from pathlib              import Path
 from datetime             import datetime
-from User                 import User
+
+from pathlib              import Path
+from User                 import *
 
 import discord
+import time
 import os
 
 # intents = discord.Intents.default()
@@ -30,6 +32,7 @@ async def on_ready():
 
 @client.slash_command(name="checkin",description="check in",guild_ids=testing_guild)
 async def checkin(ctx): 
+    print(f'[*] {ctx.author.name} try to check in')
     if (ctx.author.id not in User_dict):User_dict[ctx.author.id] = User(ctx.author)
 
     if (await User_dict[ctx.author.id].checkin()):
@@ -37,8 +40,11 @@ async def checkin(ctx):
     else:  
         await ctx.respond(f"you didn't check out last time")
 
+
+
 @client.slash_command(name="checkout",description="check out",guild_ids=testing_guild)
 async def checkout(ctx): 
+    print(f'[*] {ctx.author.name} try to check out')
     if (ctx.author.id not in User_dict):User_dict[ctx.author.id] = User(ctx.author)
 
     if (await User_dict[ctx.author.id].checkout()):
@@ -46,6 +52,23 @@ async def checkout(ctx):
     else:
         await ctx.respond(f"No check in record!!")
 
+
+@client.slash_command(name="order_drink",description="Order a drink",guild_ids=testing_guild)
+async def order_drink(ctx):
+
+    await ctx.send('Menu of drinks')
+    with open("menu.png", "rb") as fh:
+        f = discord.File(fh, filename='menu.png')
+    await ctx.send(file=f)
+
+
+    OTOM = OrderTimeOutModal(title='Time for everyone to order drinks')
+    await ctx.response.send_modal(OTOM)
+    print(f"[*] {ctx.author.name} initialize a drink order with timeout :",OTOM.timeout )
+
+    # timeout=OTOM.OrderTimeOutModal
+    # print("[*] Finished")
+    # await ctx.send("Press the button!", view=MyView())
 
 
 DISCORDTOKEN = ''
--- User.py
+++ User.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+import discord
+
 ## handle every users data
 
 class User:
@@ -44,15 +46,57 @@ class User:
         return self.__check_in_record
 
     def get_user_check_in_record(self):
-        return self.__check_ou_record
+        return self.__check_out_record
+
+
+
+class Drink_modal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.add_item(discord.ui.InputText(label="飲料品項"))
+        self.add_item(discord.ui.InputText(label="客製化(甜度冰塊)", style=discord.InputTextStyle.long))
+
+    async def callback(self, interaction: discord.Interaction):
+        embed = discord.Embed(title="你的飲料")
+        embed.add_field(name="飲料品項"        , value=self.children[0].value)
+        embed.add_field(name="客製化(甜度冰塊)" , value=self.children[1].value)
+
+        await interaction.user.send(embeds=[embed])
+        await interaction.response.send_message(content='You have successfully order your drink, please check your message', ephemeral=True)
+
+class OrderTimeOutModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.timeout = 300
+        self.ever_called =False
+        self.add_item(discord.ui.InputText(label="訂飲料時間(s)"))
+
+    async def callback(self, interaction: discord.Interaction):
+        self.timeout   =  eval(self.children[0].value)
+        # await interaction.response.send_message(content=f'You have set your timeout : {self.timeout}', ephemeral=True)
+        self.ever_called = True
+        
+        await interaction.response.send_message(f"A drink order is initiated!! {self.timeout}[s]", view=OrderDrink(timeout=self.timeout))
+
+class OrderDrink(discord.ui.View):
+
+    async def on_timeout(self):
+        for child in self.children:
+            child.disabled = True
+        await self.message.edit(content="You took too long! Disabled all the components.", view=self)
+
+
+    @discord.ui.button(label="我要訂飲料!!", style=discord.ButtonStyle.primary)
+    async def first_button_callback(self, button, interaction):
+        # await interaction.user.send('Please enter the drink you want to drink')
+        modal = Drink_modal(title='Please enter the drink you want to drink')
+        await interaction.response.send_modal(modal)
+        # await interaction.response.send(f"A drink order is initiated !!", view=self)
 
 
 
 
 if __name__ == '__main__':
     a = User()
-    print(a.get_user_check_in_record())
-    try:
-        print(a.__check_in_record)
-    except Exception as e:
-        print(e)
+
